2bc5ec21cbc49e25ca81dd91cb02a4cf8fd937ef
Jhonny Pirela (jhosagid) <jhosagid7@gmail.com>
2024-11-09 16:52:25 -0400

    feat: :fire: Add model order

---
+++ app/Models/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = ['total', 'discount', 'items', 'customer_id', 'user_id', 'status', 'notes'];
+    public function details()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
