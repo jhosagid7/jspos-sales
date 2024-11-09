b539b9d5372ef353d792e69177e971c3347d38c8
Jhonny Pirela (jhosagid) <jhosagid7@gmail.com>
2024-11-09 16:52:25 -0400

    feat: :fire: Add model order

---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,24 @@
+<?php
+
+// OrderDetail.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'regular_price', 'sale_price', 'discount'];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
