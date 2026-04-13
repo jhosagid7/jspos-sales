8ed8afe2f6e715ce9296fc50608bca8b0d20c3ba
Jhosagid7 <jhosagid7@gmail.com>
2026-04-13 08:25:20 -0400

    Release v1.9.88: Fix syntax error in AuthController (missing semicolon)

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -13,11 +13,7 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-            'device_name' => 'required'
-        ]);
+        $request->validate(['email' => 'required|email','password' => 'required','device_name' => 'required']);
 
         $credentials = [
             'email' => trim(strtolower($request->email)),
