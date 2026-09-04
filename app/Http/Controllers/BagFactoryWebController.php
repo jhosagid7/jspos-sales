8128c51fd838bc19d9c83332fbe4c453d6703d57
Jhosagid7 <jhosagid7@gmail.com>
2026-09-04 17:45:58 -0400

    Redirect welcome and login exclusively to JSBolsas Pro dashboard

--- app/Http/Controllers/BagFactoryWebController.php
+++ app/Http/Controllers/BagFactoryWebController.php
@@ -38,7 +38,7 @@ class BagFactoryWebController extends Controller
 
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
-            return redirect()->intended(route('dashboard'));
+            return redirect()->route('dashboard');
         }
 
         return back()->withErrors([
--- routes/web.php
+++ routes/web.php
@@ -86,7 +86,9 @@ Route::get('/dashboard', function (\Illuminate\Http\Request $request) {
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('welcome', Welcome::class)->name('welcome');
+    Route::get('welcome', function () {
+        return redirect()->route('dashboard');
+    })->name('welcome');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
